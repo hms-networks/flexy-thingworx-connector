af2182e57de983ee231f77c4366e8403e57d5d05
Alexander Hawk <alha@hms.se>
2021-05-21 10:30:40 -0400

    Remove old TWApiManager logic

--- src/com/hms_networks/americas/sc/thingworx/data/TWApiManager.java
+++ src/com/hms_networks/americas/sc/thingworx/data/TWApiManager.java
@@ -2,17 +2,12 @@ package com.hms_networks.americas.sc.thingworx.data;
 
 import com.ewon.ewonitf.EWException;
 import com.ewon.ewonitf.ScheduledActionManager;
-import com.hms_networks.americas.sc.datapoint.DataPoint;
 import com.hms_networks.americas.sc.fileutils.FileAccessManager;
 import com.hms_networks.americas.sc.logging.Logger;
 import com.hms_networks.americas.sc.thingworx.TWConnectorConsts;
 import com.hms_networks.americas.sc.thingworx.TWConnectorMain;
-import com.hms_networks.americas.sc.thingworx.utils.TWTimeOffsetCalculator;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Iterator;
 
 /**
  * Class for managing HTTP API calls to the Thingworx API.
@@ -42,13 +37,6 @@ public class TWApiManager {
   private static final String CONNECTION_ERROR_STRING_RESPONSE = "ConnectionError";
 
   /**
-   * Array list of data points that are waiting to be sent to Thingworx.
-   *
-   * @since 1.1
-   */
-  private static ArrayList pendingDataPoints = new ArrayList();
-
-  /**
    * Gets the name of the Ewon Flexy as it appears/should appear in Thingworx.
    *
    * @return Thingworx device name
@@ -56,16 +44,6 @@ public class TWApiManager {
    */
   public static String getApiDeviceName() {
     return "FLEXY-" + TWConnectorConsts.EWON_SERIAL_NUMBER;
-  }
-
-  /**
-   * Adds the specified data point to the list of data points to be sent to Thingworx.
-   *
-   * @param dataPoint data point to send to Thingworx
-   * @since 1.1
-   */
-  public static synchronized void addDataPointToPending(DataPoint dataPoint) {
-    pendingDataPoints.add(dataPoint);
   }
 
   /**
@@ -111,116 +89,6 @@ public class TWApiManager {
     }
     Logger.LOG_DEBUG("Thingworx HTTP POST response: " + response);
     return isSuccessful;
-  }
-
-  /**
-   * Builds the JSON array of data points and appends it to the specified string buffer.
-   *
-   * @since 1.1
-   * @param stringBuffer JSON string buffer
-   */
-  private static synchronized void buildDataPointsJson(StringBuffer stringBuffer) {
-    // Add opening for data points object
-    stringBuffer.append("\"datapoints\": [");
-
-    // Append each pending data point
-    Iterator pendingDataPointsIterator = pendingDataPoints.iterator();
-    while (pendingDataPointsIterator.hasNext()) {
-      // Get current data point
-      DataPoint currentDataPoint = (DataPoint) pendingDataPointsIterator.next();
-      pendingDataPointsIterator.remove();
-
-      // Get time stamp in proper format
-      long timestampLong =
-          Long.valueOf(currentDataPoint.getTimeStamp()).longValue()
-              * TWConnectorConsts.NUM_MILLISECONDS_PER_SECOND;
-      String currentDataPointFormattedTimestamp =
-          TWConnectorConsts.THINGWORX_API_DATE_FORMAT.format(new Date(timestampLong));
-
-      // Append data point to data points list
-      stringBuffer.append("{");
-      stringBuffer.append("\"name\": \"").append(currentDataPoint.getTagName()).append("\",");
-      stringBuffer.append("\"value\": ").append(currentDataPoint.getValueString()).append(",");
-      stringBuffer
-          .append("\"type\": ")
-          .append(currentDataPoint.getType().getRawDataType())
-          .append(",");
-      stringBuffer
-          .append("\"quality\": ")
-          .append(currentDataPoint.getQuality().getRawDataQuality())
-          .append(",");
-      stringBuffer
-          .append("\"timestamp\": \"")
-          .append(currentDataPointFormattedTimestamp)
-          .append("\"");
-      stringBuffer.append("}");
-
-      // Append comma if there are more data points to be added
-      if (pendingDataPointsIterator.hasNext()) {
-        stringBuffer.append(",");
-      }
-    }
-
-    // Add closing for data points object
-    stringBuffer.append("], ");
-  }
-
-  /**
-   * Builds a payload containing the data points that are pending and sends them to Thingworx. This
-   * task is performed on a thread that is spawned to avoid an overload of the main thread.
-   *
-   * @since 1.1
-   */
-  public static synchronized void sendPendingToThingworx() {
-    // Build runnable to send pending data points to Thingworx
-    Runnable sendPendingRunnable =
-        new Runnable() {
-          public void run() {
-
-            final int pendingDataPointsCount = pendingDataPoints.size();
-            if (pendingDataPointsCount > 0) {
-
-              // Create string buffer for building payload
-              StringBuffer payloadBuffer = new StringBuffer();
-
-              // Add opening JSON bracket
-              payloadBuffer.append("{\"Tags\":{");
-
-              // Add data points array
-              buildDataPointsJson(payloadBuffer);
-
-              // Add opening for info object
-              payloadBuffer.append("\"info\": {");
-
-              // Append Ewon name in info object
-              payloadBuffer.append("\"ewon-name\": \"").append(getApiDeviceName()).append("\",");
-
-              // Append Ewon time offset from UTC in milliseconds
-              payloadBuffer
-                  .append("\"ewon-utc-offset-millis\": \"")
-                  .append(TWTimeOffsetCalculator.getTimeOffsetMilliseconds())
-                  .append("\"");
-
-              // Add closing for info object
-              payloadBuffer.append("}");
-
-              // Add closing JSON bracket
-              payloadBuffer.append("}}");
-
-              // Send to Thingworx
-              Logger.LOG_DEBUG(
-                  "Sending HTTP POST request to Thingworx with "
-                      + pendingDataPointsCount
-                      + " data points.");
-              sendJsonToThingworx(payloadBuffer.toString());
-            }
-          }
-        };
-
-    // Create new thread and run created runnable
-    Thread sendPendingThread = new Thread(sendPendingRunnable);
-    sendPendingThread.start();
-    Logger.LOG_DEBUG("A send pending data points thread has been created and started.");
   }
 
   /**
