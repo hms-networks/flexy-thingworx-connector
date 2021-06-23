3da93b597d95d40667adf3cfd280bf5a8ceb7926
Alex Hawk <alha@hms.se>
2021-06-23 13:52:20 -0400

    Increment package version

--- src/com/hms_networks/americas/sc/thingworx/TWConnectorConsts.java
+++ src/com/hms_networks/americas/sc/thingworx/TWConnectorConsts.java
@@ -17,7 +17,7 @@ public class TWConnectorConsts {
   public static final String CONNECTOR_NAME = "Ewon Thingworx Connector";
 
   /** Version of the connector application */
-  public static final String CONNECTOR_VERSION = "2.0";
+  public static final String CONNECTOR_VERSION = "2.0.1";
 
   // HTTP Configuration
   /**
